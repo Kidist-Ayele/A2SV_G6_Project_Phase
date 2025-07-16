7ba31d2add804c18853bb4f1475e90fbc0e49fcd
Kidist_Ayele <kidistayele37@gmail.com>
2025-07-16 23:07:31 +0300

    other update

--- task-4/src/components/ToDoItem.tsx
+++ task-4/src/components/ToDoItem.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
 interface ToDoItemProps {
   todo: Todo;
   onChange: (todo: Todo) => void;
@@ -8,26 +14,11 @@ interface ToDoItemProps {
 
 function ToDoItem({ todo, onChange, onDelete }: ToDoItemProps) {
   const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title); 
 
-  let todoContent;
-
-  if (isEditing) {
-    todoContent = (
-      <>
-        <input
-          value={todo.title}
-          onChange={(e) => onChange({ ...todo, title: e.target.value })}
-        />
-        <button onClick={() => setIsEditing(false)}>Save</button>
-      </>
-    );
-  } else {
-    todoContent = (
-      <>
-        {todo.title}
-        <button onClick={() => setIsEditing(true)}>Edit</button>
-      </>
-    );
+  function handleSave() {
+    onChange({ ...todo, title: editedTitle }); 
+    setIsEditing(false);
   }
 
   return (
@@ -37,7 +28,27 @@ function ToDoItem({ todo, onChange, onDelete }: ToDoItemProps) {
         checked={todo.done}
         onChange={(e) => onChange({ ...todo, done: e.target.checked })}
       />
-      {todoContent}
+
+      {isEditing ? (
+        <>
+          <input
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)} 
+          />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+            {todo.title}
+          </span>
+          <button onClick={() => {
+            setEditedTitle(todo.title); 
+            setIsEditing(true);
+          }}>Edit</button>
+        </>
+      )}
+
       <button onClick={() => onDelete(todo.id)}>Delete</button>
     </label>
   );
